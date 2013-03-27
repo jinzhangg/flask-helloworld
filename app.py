344d44e35310d5716a656bdc7d9368416cee1fa8
Jin Zhang <jinzhang273@gmail.com>
2013-03-27 14:49:23 -0700

    added User model

--- app.py
+++ app.py
@@ -15,6 +15,31 @@ app.config.update(
 
 db = SQLAlchemy(app)
 
+
+#----------------------------------------
+# Models
+#----------------------------------------
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Create a test user
+'''
+user = User('John Doe', 'john@example.com')
+db.session.add(user)
+db.session.commit()
+'''
+
+
 #----------------------------------------
 # Controllers
 #----------------------------------------
